3ef27f378a802f4511b9fd2b824817a78550c337
rolex9799 <rolandsrepetto3@inbox.lv>
2025-02-06 17:45:54 +0100

    new commit

---
+++ Java practice/Main.java
@@ -0,0 +1,14 @@
+
+public class Main{
+    public static void main(String[] args) {
+        
+        // First Java code for me
+
+        /*
+         * Multi-line comment
+         */
+        System.out.println("Hello World!");
+        System.out.println("I like meringhatta in Ponte Tresa!");
+        System.out.println("I enjoy Lugano!");
+    }
+}
